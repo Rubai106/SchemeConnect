5423c736dcf6fb280463260985a427a4b0a1530f
Rubai106 <idkmanhehe22@gmail.com>
2026-07-31 22:29:51 +0600

    Add common middleware

---
+++ src/middleware/requestLogger.js
@@ -0,0 +1,12 @@
+const requestLogger = (req, res, next) => {
+    const startedAt = Date.now();
+
+    res.on("finish", () => {
+        const responseTime = Date.now() - startedAt;
+        console.log(`${req.method} ${req.originalUrl} ${res.statusCode} - ${responseTime}ms`);
+    });
+
+    next();
+};
+
+module.exports = requestLogger;
